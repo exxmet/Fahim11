95954446e684cfe91f73cda43343c493d1905081
exxmet <fi9089298@gmail.com>
2025-08-22 15:08:54 +0600

    Update 😤.js

--- scripts/commands/😤.js
+++ scripts/commands/😤.js
@@ -19,7 +19,7 @@ handleEvent: function({ api, event, client, __GLOBAL }) {
 	if (body.indexOf("😤")==0 || body.indexOf("🤬")==0 || body.indexOf("😒")==0 || body.indexOf("😈")==0) {
 		var msg = {
 				body: "fahim islam 😚🥀",
-				attachment: fs.createReadStream(__dirname + `/Nayan/`)
+				attachment: fs.createReadStream(__dirname + `/Nayan/ff.mp3`)
 			}
 			api.sendMessage( msg, threadID, messageID);
     api.setMessageReaction("😤", event.messageID, (err) => {}, true)
